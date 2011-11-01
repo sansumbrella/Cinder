d607f8def40a1ecec2bbe9892d21a8989c425c32
Robert Hodgin <robert@flight404.com>
2011-10-31 23:28:07 -0700

    playing with sliding camera effect

--- samples/_timeline/VisualDictionary/src/VisualDictionaryApp.cpp
+++ samples/_timeline/VisualDictionary/src/VisualDictionaryApp.cpp
@@ -16,7 +16,7 @@ using namespace std;
 class VisualDictionaryApp : public AppBasic {
   public:
 	void prepareSettings( Settings *settings );
-	void layoutWords( vector<string> words, float radius );	
+	void layoutWords( vector<string> words, Vec2f center, float radius );	
 	void setup();
 
 	void enableSelections() { mEnableSelections = true; }
@@ -31,6 +31,9 @@ class VisualDictionaryApp : public AppBasic {
 
 	list<WordNode>::iterator	getNodeAtPoint( const Vec2f &point );
 	
+	Anim<Vec2f>					mCenterPoint;
+	Anim<ColorA>				mBgColor;
+	
 	shared_ptr<Dictionary>		mDictionary;
 	list<WordNode>				mNodes, mDyingNodes;
 	list<WordNode>::iterator	mMouseOverNode;
@@ -41,26 +44,32 @@ class VisualDictionaryApp : public AppBasic {
 
 void VisualDictionaryApp::prepareSettings( Settings *settings )
 {
-	settings->setWindowSize( 800, 600 );
+	settings->setWindowSize( 1024, 1024 );
 }
 
-void VisualDictionaryApp::layoutWords( vector<string> words, float radius )
+void VisualDictionaryApp::layoutWords( vector<string> words, Vec2f center, float radius )
 {
 	int radiusDivisor = std::max<int>( 10, words.size() ); // don't let the circles get too small
 	mCurrentCircleRadius = radius / radiusDivisor * M_PI;
 	for( size_t w = 0; w < words.size(); ++w ) {
 		float angle = w / (float)words.size() * 2 * M_PI;
-		Vec2f pos = getWindowCenter() + radius * Vec2f( cos( angle ), sin( angle ) );
+		Vec2f pos = center + radius * Vec2f( cos( angle ), sin( angle ) );
+		Color col(  CM_HSV, w / (float)words.size(), 1, 1 );
 		mNodes.push_back( WordNode( words[w], false ) );
-		mNodes.back().mColor = Color( CM_HSV, w / (float)words.size(), 1, 1 );
-		mNodes.back().mPos = getWindowCenter();
-		timeline().apply( &mNodes.back().mPos, pos, 0.5f, EaseOutAtan( 10 ) ).timelineEnd( -0.45f );
+		mNodes.back().mPos = center;
+		mNodes.back().mColor = ColorA( col, 0.0f );
+		
 		timeline().apply( &mNodes.back().mRadius, mCurrentCircleRadius, 0.5f, EaseOutAtan( 10 ) ).timelineEnd( -0.5f );
+		timeline().apply( &mNodes.back().mPos, pos, 0.5f, EaseOutAtan( 10 ) ).timelineEnd( -0.475f );
+		timeline().apply( &mNodes.back().mColor, ColorA( col, 1.0f ), 0.5f, EaseOutAtan( 10 ) ).timelineEnd( -0.475f );
 	}
 }
 
 void VisualDictionaryApp::setup()
 {
+	mCenterPoint = getWindowCenter();
+	mBgColor = ColorA( 0.0f, 0.0f, 0.2f, 1.0f );
+	
 	// load the dictionary
 	mDictionary = shared_ptr<Dictionary>( new Dictionary( loadResource( "EnglishDictionary.gz" ) ) );
 
@@ -72,7 +81,7 @@ void VisualDictionaryApp::setup()
 	for( char c = 0; c < 26; ++c )
 		initialWords.push_back( string( 1, (char)('a' + c) ) );
 
-	layoutWords( initialWords, 250 );
+	layoutWords( initialWords, mCenterPoint, getWindowHeight()*0.35f );
 	
 	// mark our currently highlighted node as "none"
 	mMouseOverNode = mNodes.end();
@@ -90,45 +99,52 @@ list<WordNode>::iterator VisualDictionaryApp::getNodeAtPoint( const Vec2f &point
 	return mNodes.end();
 }
 
+
+void VisualDictionaryApp::keyDown( KeyEvent event )
+{
+	if( isalpha( event.getChar() ) ){
+		// see if we can find a word that ends with this letter
+		list<WordNode>::iterator foundWord = mNodes.end();
+		for( foundWord = mNodes.begin(); foundWord != mNodes.end(); ++foundWord ) {
+			if( foundWord->getWord()[foundWord->getWord().size()-1] == event.getChar() )
+				break;
+		}
+		
+		if( foundWord != mNodes.end() )
+			selectNode( foundWord );
+	} else {
+		if( event.getCode() == KeyEvent::KEY_BACKSPACE ){
+			
+		}
+	}
+}
+
 void VisualDictionaryApp::mouseDown( MouseEvent event )
 {
-	list<WordNode>::iterator clickedNode = getNodeAtPoint( event.getPos() );
+	list<WordNode>::iterator clickedNode = getNodeAtPoint( event.getPos() - getWindowCenter() + (Vec2f)mCenterPoint );
 	if( clickedNode != mNodes.end() )
 		selectNode( clickedNode );
 }
 
-void VisualDictionaryApp::keyDown( KeyEvent event )
-{
-	// see if we can find a word that ends with this letter
-	list<WordNode>::iterator foundWord = mNodes.end();
-	for( foundWord = mNodes.begin(); foundWord != mNodes.end(); ++foundWord ) {
-		if( foundWord->getWord()[foundWord->getWord().size()-1] == event.getChar() )
-			break;
-	}
-	
-	if( foundWord != mNodes.end() )
-		selectNode( foundWord );
-}
 
 void VisualDictionaryApp::mouseMove( MouseEvent event )
 {
 	if( ! mEnableSelections )
 		return;
 	
-	list<WordNode>::iterator currentMouseOver = getNodeAtPoint( event.getPos() );
+	list<WordNode>::iterator currentMouseOver = getNodeAtPoint( event.getPos() - getWindowCenter() + (Vec2f)mCenterPoint );
 
 	if( currentMouseOver != mMouseOverNode ) {
 		mMouseOverNode = currentMouseOver;
 		
 		// make all the circles not moused-over small, and the mouse-over big
-		float smallRadius = ( mMouseOverNode != mNodes.end() ) ? 25 : 30;
 		for( list<WordNode>::iterator nodeIt = mNodes.begin(); nodeIt != mNodes.end(); ++nodeIt ) {
 			if( mMouseOverNode == mNodes.end() )
 				timeline().apply( &nodeIt->mRadius, mCurrentCircleRadius, 0.25f, EaseOutAtan( 10 ) );
 			else if( mMouseOverNode == nodeIt )
-				timeline().apply( &nodeIt->mRadius, mCurrentCircleRadius * 2, 0.25f, EaseOutAtan( 10 ) );
+				timeline().apply( &nodeIt->mRadius, mCurrentCircleRadius * 1.35f, 0.25f, EaseOutAtan( 10 ) );
 			else
-				timeline().apply( &nodeIt->mRadius, smallRadius, 0.5f, EaseOutAtan( 10 ) );
+				timeline().apply( &nodeIt->mRadius, mCurrentCircleRadius, 0.5f, EaseOutAtan( 10 ) );
 		}
 	}
 }
@@ -146,16 +162,25 @@ void VisualDictionaryApp::selectNode( list<WordNode>::iterator selectedNode )
 	}
 	
 	mCurrentNode = *selectedNode;
+
 	mNodes.clear();
+
+
+	// move the centerpoint to the selected node's position
+	timeline().apply( &mCenterPoint, (Vec2f)mCurrentNode.mPos, 1.0f, EaseOutAtan( 10 ) );
+	timeline().apply( &mBgColor, ColorA( mCurrentNode.mColor().r * 0.5f, mCurrentNode.mColor().g * 0.5f, mCurrentNode.mColor().b * 0.5f, 1.0f ), 1.0f, EaseOutAtan( 10 ) );
 	
 	// move the selected node to the center and make it big, transparent/white
-	timeline().apply( &mCurrentNode.mRadius, 60.0f, 1.0f, EaseOutAtan( 10 ) );
-	timeline().apply( &mCurrentNode.mPos, getWindowCenter(), 0.5f, EaseOutAtan( 10 ) );
+	timeline().apply( &mCurrentNode.mRadius, 96.0f, 1.0f, EaseOutAtan( 10 ) );
+//	timeline().apply( &mCurrentNode.mPos, mCenterPoint, 0.5f, EaseOutAtan( 10 ) );
 	timeline().apply( &mCurrentNode.mColor, ColorA( 1, 1, 1, 0 ), 0.5f, EaseOutAtan( 10 ) );
 
 	// now add all the descendants of the clicked node
 	vector<string> children( mDictionary->getDescendants( mCurrentNode.getWord() ) );
-	layoutWords( children, 250 );
+	layoutWords( children, (Vec2f)mCurrentNode.mPos, getWindowHeight()*0.35f );
+	
+	// mark our currently highlighted node as "none"
+	mMouseOverNode = mNodes.end();
 	
 	// once everything is done animating, then we can allow selections, but for now, disable them
 	mEnableSelections = false;
@@ -170,17 +195,31 @@ void VisualDictionaryApp::update()
 
 void VisualDictionaryApp::draw()
 {
-	gl::clear( Color( 0.0f, 0.0f, 0.2f ) ); 
-	
+	gl::clear( mBgColor ); 
 	gl::enableAlphaBlending();
 	
-	for( list<WordNode>::const_iterator nodeIt = mNodes.begin(); nodeIt != mNodes.end(); ++nodeIt )
-		nodeIt->draw();
+	gl::pushMatrices();
+	gl::translate( getWindowCenter() - mCenterPoint );
+	
+	// draw the dying nodes
 	for( list<WordNode>::const_iterator nodeIt = mDyingNodes.begin(); nodeIt != mDyingNodes.end(); ++nodeIt )
 		nodeIt->draw();
-		
+	
+	// draw all the non-mouseOver nodes
+	for( list<WordNode>::const_iterator nodeIt = mNodes.begin(); nodeIt != mNodes.end(); ++nodeIt ){
+		if( nodeIt != mMouseOverNode )
+			nodeIt->draw();
+	}
+	
+	// if there is a mouseOverNode, draw it last so it is 'above' the others
+	if( mMouseOverNode != mNodes.end() )
+		mMouseOverNode->draw();
+	
+	// if there is a currentNode (previously selected), draw it	
 	if( ! mCurrentNode.getWord().empty() )
 		mCurrentNode.draw();
+	
+	gl::popMatrices();
 }
 
 
--- samples/_timeline/VisualDictionary/src/WordNode.cpp
+++ samples/_timeline/VisualDictionary/src/WordNode.cpp
@@ -39,8 +39,12 @@ bool WordNode::shouldBeDeleted() const
 
 void WordNode::draw() const
 {
-	gl::color( mColor );
+	gl::color( mColor() * 0.3f );
 	gl::drawSolidCircle( mPos, mRadius );
+
+	
+	gl::color( mColor );
+	gl::drawSolidCircle( mPos, mRadius - 3.0f );
 
 	// biggest square that can fit in the circle is radius * sqrt(2) per side  x^2 = (r^2)/2
 	const float squareSide = sqrtf( mRadius * mRadius / 2.0f ) * 2;
@@ -49,8 +53,8 @@ void WordNode::draw() const
 
 	const Vec2f offset = (Vec2f)mPos + Vec2f( -mRadius + ( mRadius * 2 - mWordPixelLength * pixelScale ) / 2, mRadius - (mRadius * 2.0f - 60 * pixelScale ) / 2 );
 
-	gl::color( Color::black() );
-	sFont->drawString( mWord, offset + Vec2f( 1, 1 ) * pixelScale, options );
+	gl::color( ColorA( Color::black(), 0.5f ) );
+	sFont->drawString( mWord, offset + Vec2f( pixelScale, pixelScale ) * 2.0f, options );
 	gl::color( Color::white() );
 	sFont->drawString( mWord, offset, options );
 }
