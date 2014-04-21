ab732edeee6c5279de81484196d15e4718150aee
petroskataras <petroskataras@gmail.com>
2014-04-21 16:03:52 +0200

    Adds ListenerBasic example.

--- samples/ListenerBasic/include/SquareListener.h
+++ samples/ListenerBasic/include/SquareListener.h
@@ -1,6 +1,12 @@
 #pragma once
 
-#include "cinder/App/App.h"
+// Case sensitive file system
+#ifdef __linux
+    #include "cinder/app/App.h"
+#else
+    #include "cinder/App/App.h"
+#endif
+
 #include "cinder/Vector.h"
 
 class SquareListener {
