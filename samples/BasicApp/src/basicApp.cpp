37f68e3e96e7772fedd18c0b2c1808643deaba7b
petroskataras <petroskataras@gmail.com>
2014-04-26 22:33:59 +0200

    BasicApp working with glfw3

--- samples/BasicApp/src/basicApp.cpp
+++ samples/BasicApp/src/basicApp.cpp
@@ -9,6 +9,8 @@ using namespace std;
 class BasicApp : public AppBasic {
   public:
 	void mouseDrag( MouseEvent event );
+    void mouseUp( MouseEvent event );
+    void mouseDown( MouseEvent event );
 	void keyDown( KeyEvent event );
 	void draw();
 
@@ -16,6 +18,36 @@ class BasicApp : public AppBasic {
 	list<Vec2f>		mPoints;
 };
 
+void BasicApp::mouseUp( MouseEvent event )
+{
+}
+void BasicApp::mouseDown( MouseEvent event )
+{
+    if( event.isShiftDown() )
+    {
+        std::cout << " Shift isPressed ! " << std::endl;
+    }
+    if( event.isControlDown() )
+    {
+        std::cout << " Control isPressed ! " <<std::endl;
+    }
+     if( event.isLeftDown() )
+    {
+        std::cout << " Mouse LEFT isPressed " << std::endl;
+    }
+    if( event.isMiddleDown() )
+    {
+        std::cout << " MOUSE MIDDLE isPressed " << std::endl;
+    }
+   if( event.isRightDown() )
+    {
+        std::cout << " MOUSE RIGHT isPressed" << std::endl;
+    }
+    if( event.isAltDown() )
+    {
+        std::cout << " ALT isPressed " << std::endl;
+    }
+}
 void BasicApp::mouseDrag( MouseEvent event )
 {
 	mPoints.push_back( event.getPos() );
@@ -23,6 +55,7 @@ void BasicApp::mouseDrag( MouseEvent event )
 
 void BasicApp::keyDown( KeyEvent event )
 {
+    std::cout << " KEY DOWN " << std::endl;
 	if( event.getChar() == 'f' )
 		setFullScreen( ! isFullScreen() );
 }
